0589b96db4a199a4b742ad5bf599bac8ba68384a
Timo Paulssen <timonator@perpetuum-immobile.de>
2009-02-01 21:49:18 +0100

    added first draft of database population function.

--- quest/models.py
+++ quest/models.py
@@ -1,7 +1,12 @@
 from elixir import *
 from sqlalchemy import MetaData
+import datetime
 
 __metadata__ = MetaData()
+
+class Room(Entity):
+    name     = Field(Unicode(6), primary_key=True)
+    doors    = ManyToMany("Room")
 
 class Player(Entity):
     """This class represents a player as well as a session (one session per player)"""
@@ -10,50 +15,48 @@ class Player(Entity):
     
     join_date = Field(DateTime, default=datetime.datetime.now)
 
-    status    = Field(SmallInt)
+    status    = Field(Integer)
     login     = Field(DateTime)
 
-    position  = ForeignKey(Room, deferred="session")
+    position  = ManyToOne(Room)
     
-    health    = Field(Integer, deferred="stats", default=1000)
-    maxHealth = Field(Integer, deferred="stats", default=1000)
-    gold      = Field(Integer, deferred="stats", default=0)
-    fluency   = Field(Integer, deferred="stats", default=1)
-    cmavobag  = Field(Integer, deferred="stats", default=100)
-    gismubag  = Field(Integer, deferred="stats", default=50)
-    maxsenlen = Field(Integer, deferred="stats", default=10)
-    dexterity = Field(Integer, deferred="stats", default=1)
-
-    
+    health    = Field(Integer, default=1000)
+    maxHealth = Field(Integer, default=1000)
+    gold      = Field(Integer, default=0)
+    fluency   = Field(Integer, default=1)
+    cmavobag  = Field(Integer, default=100)
+    gismubag  = Field(Integer, default=50)
+    maxsenlen = Field(Integer, default=10)
+    dexterity = Field(Integer, default=1)
 
     def __repr__(self):
         return u'<Player "%s" (%sline)>' % (self.username, [u"On", u"Off"][self.status])
-
-class Room(Entity):
-    name     = Field(Unicode(6), primary_key=True)
-    doors    = ManyToMany(Room)
 
 ### things for the treasure chest game and combat
 
 class HumanSentence(Entity):
     text     = Field(UnicodeText, primary_key=True)
-    author   = ForeignKey(Player) # if someone uses an "old" sentence, the damage will be halved and it will not be recorded again.
+    author   = ManyToOne(Player) # if someone uses an "old" sentence, the damage will be halved and it will not be recorded again.
     score    = Field(Integer)
     reviews  = Field(Integer)
 
 class Selmaho(Entity):
     """This class is used for letting game admins/BPFK members assign bonuses for selmaho"""
     selmaho  = Field(Unicode(12), primary_key=True)
-    bonus    = Field(Float)
+    multi    = Field(Float)
+
+    def __repr__(self):
+        return "<Selmaho '%s' multiplier %f>" % (self.selmaho, self.multi)
 
 class WordCard(Entity):
     word       = Field(Unicode(6), primary_key=True)
     gloss      = Field(Unicode(32))
     definition = Field(UnicodeText)
-    selmaho    = ForeignKey(Selmaho)
+    selmaho    = ManyToOne(Selmaho)
+    rafsi      = Field(Unicode(13))
 
 class WordFamiliarity(Entity):
     """This class records, wether or not a player has seen a word before."""
-    player   = ForeignKey(Player)
-    word     = ForeignKey(WordCard)
+    player   = ManyToOne(Player)
+    word     = ManyToOne(WordCard)
     count    = Field(Integer)
---
+++ quest/utils.py
@@ -0,0 +1,225 @@
+from models import *
+import nagare.database
+import re
+from sqlalchemy import or_, and_
+import sqlalchemy.orm.exc 
+import random
+
+def makeWorldGraph():
+    out = open("world.dot", "w")
+    out.write("""graph "Tersistu'as" {\n  graph [overlap=prism]\n  node [shape=box]\n""")
+
+    for room in Room.query.order_by(Room.name):
+        if len(room.doors) > 4:
+            continue
+        for other in room.doors:
+            out.write("""  "%s" -- "%s"\n""" % (room.name, other.name))
+
+    out.write("""}""")
+
+def likeLevenOne(name):
+    return and_(or_(*(Room.name.like("%s_%s" % (name[:i], name[i+1:])) for i in range(len(name)))),
+                Room.name != name)
+
+def cmavoStep(name):
+    seq1 = list("uieaoui")
+    seq2 = [list(x) for x in ["xptfsckxp", "gbdvzjgb", "nmn", "rlr"]]
+    pairs = {u'p': u'b', u'b': u'p',
+             u't': u'd', u'd': u't',
+             u'k': u'g', u'g': u'k',
+             u'f': u'v', u'v': u'f',
+             u'c': u'j', u'j': u'c',
+             u's': u'z', u'z': u's'}
+
+    isstep = lambda seq, x, y: seq.index(x) - seq.index(y) == -1
+
+    addapore = re.compile("^([bcdfgjklmnoprstuvxz]?[aeiouy])([aeiouy])$")
+
+    others = []
+    
+    # rule 1: add or omit i at the end
+    if name[-1] == "i":
+        others.append(name[:-1])
+    else:
+        others.append(name + "i")
+
+    # rule 2: adds/omits an apostrophe
+    if "'" in name:
+        others.append(name.replace("'", ""))
+    else:
+        try:
+            mat = addapore.match(name)
+            others.append(mat.group(1) + "'" + mat.group(2))
+        except: pass
+
+    # rule 3: adds/omits p at the beginning
+    if name[0] == "p":
+        others.append(name[1:])
+    else:
+        others.append("p" + name)
+
+    # rule 4: steps the last letter among seq1
+    try:
+        others.append(name[:-1] + seq1[seq1.index(name[-1]) + 1])
+    except: pass
+
+    # rule 5: steps the first letter among one of seq2
+    for seq in seq2:
+        if name[0] in seq:
+            others.append(seq[seq.index(name[0]) + 1] + name[1:])
+
+    # rule 6: switches the consonant from voiced to unvoiced or vice versa
+    try:
+        others.append(pairs[name[0]] + name[1:])
+    except: pass
+
+    return and_(or_(*(Room.name == other for other in others)),
+                Room.name != name)
+
+def populate_db():
+    defire = re.compile("(.*?);")
+    print "will now populate the database."
+
+    print "First step: valsi"
+    print "loading wordlists"
+    print
+
+    print "gismu and rafsi-cmavo:"
+
+    lines = open("gismu.txt").readlines()[1:]
+
+    num = 0
+    count = len(lines)
+
+    def make_or_get_selmaho(selmaho):
+        try:
+            sm = session.query(Selmaho).filter_by(selmaho = selmaho).one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            sm = Selmaho()
+            sm.selmaho = selmaho
+            sm.milti = 1
+            session.add(sm)
+
+        return sm
+
+    for valsi in lines:
+        num += 1
+
+        val = unicode(valsi[0:6].strip())
+        gloss = unicode(valsi[20:42]).strip()
+        try:
+            defi = unicode(defire.match(valsi[62:159]).group(1)).strip()
+        except:
+            defi = unicode(valsi[62:195]).strip()
+
+        rafsi = unicode(valsi[7:20]).strip()
+
+        if len(val) != 5:
+            smt = None
+        else:
+            smt = u"GISMU"
+
+        print "\r(% 5i/% 5i) %s" % (num, count, val),
+        wc = WordCard()
+        wc.word = val
+        wc.gloss = gloss
+        wc.definition = defi
+        wc.rafsi = rafsi
+
+        sm = None
+        if smt:
+            wc.selmaho = make_or_get_selmaho(smt)
+        else:
+          wc.selmaho = None
+
+        session.add(wc)
+
+    print
+    print "rafsi-less cmavo and selma'o of rafsi-cmavo"
+    print
+
+    lines = open("cmavo.txt").readlines()[1:]
+
+    count= len(lines)
+    num = 0
+
+    for valsi in lines:
+        num += 1
+        if "*" in valsi[12:20]:
+            continue
+        
+        print "\r(% 5i/% 5i) %s" % (num, count, val),
+
+        val = unicode(valsi[0:11].strip())
+        if val[0] == ".": val = val[1:]
+
+        selmaho = unicode(valsi[11:20].strip())
+        
+        oldval = WordCard.query.filter_by(word = val)
+
+        if oldval.count() != 0:
+            ov = oldval.one()
+            ov.selmaho = make_or_get_selmaho(selmaho)
+            session.add(ov)
+        else:
+            gloss = unicode(valsi[20:62].strip())
+            defi = unicode(valsi[62:168].strip())
+            rafsi = u""
+
+            wc = WordCard()
+            wc.word = val
+            wc.gloss = gloss
+            wc.definition = defi
+            wc.selmaho = make_or_get_selmaho(selmaho)
+            wc.rafsi = rafsi
+            session.add(wc)
+
+    print
+    print
+    print "Second step: rooms."
+    print
+
+    num = 0
+    count = WordCard.query.count()
+
+    for gismu in WordCard.query.order_by(WordCard.word):
+        num += 1
+        print "\r(% 5i/% 5i) %s" % (num, count, gismu.word),
+
+        room = Room()
+        room.name = gismu.word
+    
+    print
+    print
+    print "Third step: connecting rooms."
+    print
+
+    num = 0
+
+    for theroom in Room.query.order_by(Room.name):
+        num += 1
+        
+        rafsi = WordCard.query.filter(WordCard.word == theroom.name).one().rafsi.split()
+        
+        print "\r(% 5i/% 5i) %s - %s                          " % (num, count, theroom.name, rafsi),
+
+        # is this a gismu or is this a rafsi?
+
+
+        if WordCard.query.filter(WordCard.word == theroom.name).one().selmaho.selmaho == "GISMU":
+            adjacentrooms = Room.query.filter(likeLevenOne(theroom.name))
+            dungeondoor = Room.query.filter(Room.name.in_(rafsi))
+            l = list(adjacentrooms)
+            random.shuffle(l)
+            l = l[:random.choice([3, 4])]
+            if dungeondoor.first():
+                l.append(dungeondoor.first())
+        else:
+            adjacentrooms = Room.query.filter(cmavoStep(theroom.name))
+            l = list(adjacentrooms)
+
+        for other in l:
+            theroom.doors.append(other)
+            other.doors.append(theroom)
+
+    makeWorldGraph()
